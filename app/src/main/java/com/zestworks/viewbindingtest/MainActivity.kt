066dfbab03154cc017ea624b74e0ba129b6d84a0
prithivraj-toast <prithivraj.selvaraj@toasttab.com>
2020-02-11 11:39:29 +0000

    Inner view test

--- app/src/main/java/com/zestworks/viewbindingtest/MainActivity.kt
+++ app/src/main/java/com/zestworks/viewbindingtest/MainActivity.kt
@@ -13,9 +13,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val b = binding.helloWorld == null
-        val b1 = binding.worldHello == null
-        Log.d("helloWorld null : ", b.toString())
+        val b1 = binding.worldHello
+        val b3 = binding.innerTextView
         Log.d("worldHello null : ", b1.toString())
+        Log.d("worldHello null : ", b3.toString())
     }
 }
